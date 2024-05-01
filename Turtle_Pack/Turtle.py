9217b3a98893f5eecf9a35ff7954074ddb8057fd
Gustavo.M <Enderdragon3245@gmail.com>
2024-05-01 15:58:32 -0300

    Starting The Python Turtle Project

---
+++ Turtle_Pack/Turtle.py
@@ -0,0 +1,14 @@
+from turtle import *
+turtle = Turtle()
+
+turtle.color('blue')
+
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.backward(100)
+
+
+mainloop()
