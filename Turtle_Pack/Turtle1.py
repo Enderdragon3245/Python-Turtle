26f344f034f509c0ec6d51d3dc49c39f76959f3a
Gustavo.M <Enderdragon3245@gmail.com>
2024-05-01 16:04:33 -0300

    ClearScrean And several other functions

---
+++ Turtle_Pack/Turtle1.py
@@ -0,0 +1,15 @@
+from turtle import *
+turtle = Turtle()
+turtle.pos()
+
+turtle.color("red")
+
+turtle.width(3)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+
+clearscreen()
+turtle.home()
+
+mainloop()
